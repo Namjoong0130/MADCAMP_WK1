0b8f4c61f8a5ed0f6bf9b79f31d9cc7700e504b9
Junyeop Lee <orca@sparcs.org>
2026-01-09 19:15:32 +0900

    init Screen edit

---
+++ app/src/main/java/com/example/madcamp_1/ui/screen/initial/initScreen.kt
@@ -0,0 +1,24 @@
+package com.example.madcamp_1.ui.screen.initial
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun InitScreen(
+    title: String = "대학 아레나"
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.headlineLarge
+        )
+    }
+}
